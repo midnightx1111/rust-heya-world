7152924fdb02f4385244712e0dc4981a22d1b5ae
midnightx1111 <156886938+midnightx1111@users.noreply.github.com>
2024-01-27 19:52:26 +0500

    heya world!

---
+++ heyaworld!/heya_world/target/debug/deps/heya_world-7986f6369d2aa786.d
@@ -0,0 +1,5 @@
+/home/gojosatoru/heyaworld!/heya_world/target/debug/deps/heya_world-7986f6369d2aa786: src/main.rs
+
+/home/gojosatoru/heyaworld!/heya_world/target/debug/deps/heya_world-7986f6369d2aa786.d: src/main.rs
+
+src/main.rs:
---
+++ heyaworld!/heya_world/target/debug/heya_world.d
@@ -0,0 +1,1 @@
+/home/gojosatoru/heyaworld!/heya_world/target/debug/heya_world: /home/gojosatoru/heyaworld!/heya_world/src/main.rs
